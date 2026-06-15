3af94d75cd0ea91d7ee5318844b478f1e11e5fad
ADEL HUSHAM <adelaljaale@gmail.com>
2026-06-15 22:34:37 +0800

    Fix official resources section

--- app/page.tsx
+++ app/page.tsx
@@ -186,7 +186,7 @@ export default async function HomePage() {
             </p>
           </div>
 
-          <div <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+          <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
   {officialResources.map((resource) => (
     <ResourceCard key={resource.id} resource={resource} />
   ))}
